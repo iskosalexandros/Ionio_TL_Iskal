ef65cea0371a7ea6ea179661d72b239e898af140
iskosalexandros <148728097+iskosalexandros@users.noreply.github.com>
2025-05-27 20:33:58 +0300

    Update main.py

--- TLproj/app/main.py
+++ TLproj/app/main.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Molecular Bio Pipeline", layout='wide')
 
 tabs = {
     "Analysis": show_analysis,
-    "Visualization": show_visualization,
+    #"Visualization": show_visualization,
     "About Me": show_about
 }
 choice = st.sidebar.radio("Tab Select", list(tabs.keys()))
